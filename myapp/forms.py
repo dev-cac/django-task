dfb543c6111f45c102e3806dbfecf3f2d78236ac
luisfosg <luisfosgdev@gmail.com>
2023-08-13 22:37:06 -0500

    feat: custom forms

--- myapp/forms.py
+++ myapp/forms.py
@@ -7,6 +7,18 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['name', 'description', 'project', 'important']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'project': forms.Select(attrs={'class': 'form-control'}),
+            'important': forms.CheckboxInput(
+                attrs={'class': 'form-check-input text-center'}
+            )
+        }
 
 class CreateNewProject(forms.Form):
-    name = forms.CharField(label="Titulo del Proyecto", max_length=200)
+    name = forms.CharField(
+        label="Name Project",
+        max_length=200,
+        widget= forms.TextInput(attrs={'class': 'form-control'})
+    )
